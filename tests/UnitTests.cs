f1e75f2fc7a22ab89237bbf771210ed5119de76f
tmacharia <timothy.macharia@outlook.com>
2021-05-30 02:46:10 +0300

    initial normal paginate tests

--- tests/UnitTests.cs
+++ tests/UnitTests.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using Paginator.EntityFrameworkCore;
 using NUnit.Framework;
 
 namespace tests
@@ -10,9 +12,42 @@ namespace tests
         }
 
         [Test]
-        public void Test1()
+        [Order(0)]
+        public void Normal_Paginate_Empty()
         {
-            Assert.Pass();
+            var list = Context.Cars.Paginate(1, 10);
+            Assert.IsNotNull(list);
+            Assert.Zero(list.TotalItems);
+        }
+
+        [Test]
+        [Order(1)]
+        public void Paginate_With_Items()
+        {
+            Add(new Car());
+            Add(new Car());
+            Add(new Car());
+            Add(new Car());
+
+            var list = Context.Cars.Paginate(1, 2);
+            Assert.IsNotNull(list);
+            Assert.AreEqual(4, list.TotalItems);
+            Assert.AreEqual(2, list.TotalPages);
+        }
+
+        [Test]
+        [Order(2)]
+        public void Paginate_Skip_Count()
+        {
+            Add(new Car());
+            Add(new Car());
+            Add(new Car());
+            Add(new Car());
+
+            var list = Context.Cars.Paginate(1, 2, true);
+            Assert.IsNotNull(list);
+            Assert.AreEqual(2, list.TotalItems);
+            Assert.AreEqual(1, list.TotalPages);
         }
     }
 }
